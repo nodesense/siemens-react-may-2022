7a2db0608c4b7ae85141d03b36d01cf702c22e35
Gopalakrishnan <gopalakrishnan.subramani@gmail.com>
2022-05-16 14:16:33 +0530

    env

--- src/app/redux-cart/state/services/product.services.js
+++ src/app/redux-cart/state/services/product.services.js
@@ -10,6 +10,9 @@ import axios from 'axios'
 const API_ENDPOINT = process.env.REACT_APP_END_POINT
 console.log("API END POINT IS ", API_ENDPOINT)
 
+//webpack will chage statement
+console.log("API END POINT from process.env.REACT_APP_END_POINT ", process.env.REACT_APP_END_POINT)
+
 export const fetchProducts = async () =>  {
   const resp = await  axios.get(`${API_ENDPOINT}/api/products`)
   return resp.data // data is array of products
